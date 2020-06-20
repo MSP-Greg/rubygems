24322fa08b98ca6453458543ce6b5c1814e10437
MSP-Greg <Greg.mpls@gmail.com>
2020-06-20 17:10:51 -0500

    Try again RubyGems dummy commit for CI filtering

--- test/rubygems/test_gem_ext_cmake_builder.rb
+++ test/rubygems/test_gem_ext_cmake_builder.rb
@@ -7,9 +7,6 @@ class TestGemExtCmakeBuilder < Gem::TestCase
   def setup
     super
 
-    # Details: https://github.com/rubygems/rubygems/issues/1270#issuecomment-177368340
-    # skip "CmakeBuilder doesn't work on Windows." if Gem.win_platform?
-
     begin
       _, status = Open3.capture2e('cmake')
       skip 'cmake not present' unless status.success?
